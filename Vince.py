07d399fdac70e222e89b54a4c4bc23e6393bb0d5
alexge50 <alexge50.x@gmail.com>
2017-11-18 20:15:52 +0200

    added personalities

--- Vince.py
+++ Vince.py
@@ -5,13 +5,24 @@ import json
 
 
 class Vince:  # main cog
-    def __init__(self, bot, init_data):
+    def __init__(self, bot, init_data, personalities):
         self.bot = bot
         self.name = init_data["name"]
+        self.personalities = personalities
+        self.current_personality = "normal"
 
     @commands.command(pass_context=True, no_pm=True)
     async def hi(self, ctx):
-        await self.bot.say("Hi, I am {}, nice to meet you!".format(self.name))
+        await self.bot.say(self.personalities[self.current_personality]["Vince"]["hi"].format(self.name))
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def change_personality(self, ctx, *, personality: str):
+        if personality in self.personalities:
+            self.current_personality = personality
+            key = "ok"
+        else:
+            key = "notok"
+        await self.bot.say(self.personalities[self.current_personality]["Vince"]["change_personality"][key].format(personality))
 
 
 bot = commands.Bot(command_prefix="v$")
@@ -34,7 +45,10 @@ def init(config_file):
     with open(json_config["token_file"]) as f:
         token = f.read()
 
-    vince = Vince(bot, json_config)
+    with open(json_config["personalities_file"]) as f:
+        personalities = json.load(f)
+
+    vince = Vince(bot, json_config, personalities)
     bot.add_cog(vince)
 
 
