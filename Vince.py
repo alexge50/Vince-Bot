0faed2215350516af83e56db7d53f1b742896315
alexge50 <alexge50.x@gmail.com>
2017-12-02 20:36:37 +0200

    added the new functionality

--- Vince.py
+++ Vince.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import json
 
 import modules
+import virtualbot
 
 
 class Vince(commands.Bot):
@@ -11,7 +12,6 @@ class Vince(commands.Bot):
         self.modules = None
         self.token = None
         self.personalities = None
-        self.current_personality = None
         self.name = None
 
         with open(config_file, "r") as f:
@@ -23,16 +23,26 @@ class Vince(commands.Bot):
         with open(json_config["personalities_file"]) as f:
             self.personalities = json.load(f)
 
-        self.current_personality = json_config["initial_personality"]
+        with open(json_config["modules_config_file"]) as f:
+            modules_config = json.load(f)
+
         self.name = json_config["name"]
 
         self.modules = modules.build_modules_list(self, json_config["modules_config_file"])
+        self.instance_manager = virtualbot.BotInstanceManager(json_config["database"], modules_config, json_config)
 
         for (x, y) in self.modules.items():
             self.add_cog(y)
 
     async def on_ready(self):
         print('Logged in as:\n{0} (ID: {0.id})'.format(self.user))
+
+        for server in self.servers:
+            self.instance_manager.new_instance(server.id)
+
+    async def on_server_join(self, server):
+        print("Bot was invited to {}".format(server.name))
+        self.self.instance_manager.new_instance(server.id)
 
     def run_from_config(self):
         self.run(self.token)
