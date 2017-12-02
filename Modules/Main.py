ef09cb3a821128da93bc42919cb15090797c2d87
alexge50 <alexge50.x@gmail.com>
2017-12-02 20:56:28 +0200

    updated Main module to work in the new system

--- Modules/Main.py
+++ Modules/Main.py
@@ -3,18 +3,44 @@ from discord.ext import commands
 
 
 class Main:  # main cog
-    def __init__(self, bot, config):
+    def __init__(self, bot):
         self.bot = bot
 
     @commands.command(pass_context=True, no_pm=True)
     async def hi(self, ctx):
-        await self.bot.say(self.bot.personalities[self.bot.current_personality]["Vince"]["hi"].format(self.bot.name))
+        bot_instance = self.bot.instance_manager.get_instance(ctx.message.channel.server.id)
+        await self.bot.say(self.bot.personalities[bot_instance.current_personality]["Vince"]["hi"].format(self.bot.name))
 
     @commands.command(pass_context=True, no_pm=True)
     async def change_personality(self, ctx, *, personality: str):
+        bot_instance = self.bot.instance_manager.get_instance(ctx.message.channel.server.id)
         if personality in self.bot.personalities:
-            self.bot.current_personality = personality
+            bot_instance.current_personality = personality
+            bot_instance.update()
             key = "ok"
         else:
             key = "notok"
-        await self.bot.say(self.bot.personalities[self.bot.current_personality]["Vince"]["change_personality"][key].format(personality))
+        await self.bot.say(
+            self.bot.personalities[bot_instance.current_personality]["Vince"]["change_personality"][key].format(
+                personality))
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def gdbe(self, ctx):
+        bot_instance = self.bot.instance_manager.get_instance(ctx.message.channel.server.id)
+
+        await self.bot.say("```{}```".format(bot_instance.get_database_entry()))
+
+
+class MainInstance:  # hold the properties that this module needs
+    def __init__(self, bot_instance, entry=None, config=None):
+        self.bot_instance = bot_instance
+        if entry is not None:
+            pass
+        elif config is None:
+            pass
+        else:
+            pass
+            # it doesn't need any configuration
+
+    def get_attributes(self):  # converts class' attributes to a dict, so the database can be updated
+        return dict()
