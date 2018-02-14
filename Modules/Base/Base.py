4f8f4b025dada9a43e85423e712621c4700c5f2c
alexge50 <alexge50.x@gmail.com>
2018-02-14 14:45:30 +0200

    added permission checking functions

--- Modules/Base/Base.py
+++ Modules/Base/Base.py
@@ -4,15 +4,19 @@ base class for modules
 
 import discord.ext.commands.context
 import discord.message
+from discord.permissions import Permissions
+from discord.user import User
+from discord.channel import Channel
 
 import inspect
 
 
 class Base:
-    def __init__(self, bot, resources, personality_data):
+    def __init__(self, bot, resources, permissions, personality_data):
         self.bot = bot
         self.name = self.__class__.__name__
         self.resources = resources
+        self.permissions = permissions
         self.personality_data = personality_data
 
         self.load_resources()
@@ -43,15 +47,59 @@ class Base:
     def load_resources(self):
         pass
 
-    def get_personality_data(self, bot_instance):
+    def get_personality_data(self, bot_instance, is_global=True, message_name=None):
+        if is_global:
+            return self.personality_data[bot_instance.current_personality][message_name]
+        else:
+            current_frame = inspect.currentframe()
+            caller_frame = inspect.getouterframes(current_frame, 2)
+            caller_name = caller_frame[1][3]
+
+            return self.personality_data[bot_instance.current_personality][caller_name]
+
+    def check_permissions(self, ctx: discord.ext.commands.context.Context):
         current_frame = inspect.currentframe()
         caller_frame = inspect.getouterframes(current_frame, 2)
         caller_name = caller_frame[1][3]
 
-        return self.personality_data[bot_instance.current_personality][caller_name]
+        permissions = self.permissions[caller_name]
+        user = ctx.message.author
+        channel = ctx.message.channel
+
+        return Base.check_required_permissions(user, ctx.message.channel, permissions)
+
+    @staticmethod
+    def check_required_permissions(user: User, channel: Channel, permissions_required: list):
+        user_permissions = channel.permissions_for(user)
+        user_permissions_dictionary = Base.build_permissions_dictionary(user_permissions)
+
+        check = True
+
+        for permission in permissions_required:
+            if permission == "alexge50":
+                check = check and user.id == "304170392375787520"
+            else:
+                check = check and user_permissions_dictionary[permission]
+
+        return check
 
     def help(self):
         return "", {}
+
+    @staticmethod
+    def build_permissions_dictionary(user_permissions):
+        user_permissions_dictionary = {"kick members": user_permissions.kick_members,
+                                       "ban members": user_permissions.ban_members,
+                                       "administrator": user_permissions.administrator,
+                                       "manage channels": user_permissions.manage_channels,
+                                       "manage server": user_permissions.manage_server,
+                                       "read": user_permissions.read_messages,
+                                       "write": user_permissions.send_messages,
+                                       "manage messages": user_permissions.manage_messages,
+                                       "manage nicknames": user_permissions.manage_nicknames,
+                                       "manage roles": user_permissions.manage_roles}
+        return user_permissions_dictionary
+
 
     @staticmethod
     def get_module(bot_instance, name):
@@ -63,7 +111,7 @@ class Base:
 
     @staticmethod
     def get_id_from_message(message):
-            return message.channel.server.id
+        return message.channel.server.id
 
 
 class BaseInstance:
