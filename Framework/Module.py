2a2dec3654ef414ea5eaec5a84b6a12c1ccafe3a
alexge50 <alexge50.x@gmail.com>
2018-04-20 00:30:01 +0300

    added more fields to module builder, added make_module_builder_list()

--- Framework/Module.py
+++ Framework/Module.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 from Framework import util
 
-ModuleBuilder = namedtuple('ModuleBuilder', 'name class')
+ModuleBuilder = namedtuple('ModuleBuilder', 'name class default_config resources permissions')
 
 
 def load_module_builder(json_module, directory):  # creates a ModuleBuilder
@@ -13,4 +13,19 @@ def load_module_builder(json_module, directory):  # creates a ModuleBuilder
     module_class = eval("{}.{}".format(lib,
                                        json_module["class"]))
 
-    return ModuleBuilder(json_module['name'], module_class)
+    return ModuleBuilder(json_module['name'],
+                         module_class,
+                         json_module['default_config'],
+                         json_module['resources'],
+                         json_module['permissions'])
+
+
+def make_module_builder_list(module_directory):
+    json_modules = util.load_json_file(module_directory + '/modules.json')
+
+    module_builders = []
+    for module_name in json_modules:
+        json_file = module_directory + '/' + module_name + '/' + module_name + '.json'
+        module_builders.append(load_module_builder(util.load_json_file(json_file), module_directory))
+
+    return module_builders
