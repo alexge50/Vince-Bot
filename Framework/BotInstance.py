3a36e14b2d83ee5656983efd0614787db1796cdc
alexge50 <alexge50.x@gmail.com>
2018-04-21 01:07:40 +0300

    added init for properties and corrected mistakes

--- Framework/BotInstance.py
+++ Framework/BotInstance.py
@@ -1,23 +1,31 @@
 
 class BotInstanceManager:
-    def __init__(self, bot):
+    def __init__(self, bot, default_properties):
         self.bot = bot
+        self.default_properties = default_properties
 
         self.instances = {}
 
     def add_instance(self, serverid):
+        if self.database_lookup("servers", serverid) is None:
+            self.database_update("servers", serverid, {})
+
+        for key, value in self.default_properties.items():
+            if self.database_lookup(key, serverid) is None:
+                self.database_update(key, serverid, value)
+
         self.instances[serverid] = BotInstance(self, serverid)
 
     def database_lookup(self, table, id):  # looks into database's table, for the object with a certain id
-        return self.bot.database[table].find_one(id=id)
+        return self.bot.database[table].find_one(_id=id)
 
     def database_update(self, table, id, data):  # updates row
-        data['id'] = id
+        data['_id'] = id
         with self.bot.database as db:
-            if db[table].find_one(id=id) is None:
+            if db[table].find_one(_id=id) is None:
                 db[table].insert(data)
             else:
-                db[table].update(data, ['id'])
+                db[table].update(data, ['_id'])
 
 
 class BotInstance:
