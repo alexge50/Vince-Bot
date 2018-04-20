7c8d6e4864be13647490b5ee00a5916847a4f731
alexge50 <alexge50.x@gmail.com>
2018-04-20 23:08:22 +0300

    added instance creation for modules

--- Vince.py
+++ Vince.py
@@ -1,20 +1,30 @@
-import traceback
-
-import json
-
 from discord.ext import commands
 import discord
 
-from Framework import util
+from Framework import util, Module
+from Framework.BotInstance import *
+
+import dataset
+
 
 class Vince(commands.Bot):
     def __init__(self, config_file, command_prefix, **options):
         super().__init__(command_prefix, **options)
         self.token = None
+        self.database = None
+        self.manager = BotInstanceManager(self)
+        self.module_builders = None
+        self.modules = {}
 
         config = util.load_json_file(config_file)
-        # token
-        self.token = util.load_file(config["token_file"])
+        self.token = util.load_file(config['token_file'])  # token
+        self.database = dataset.connect(config['database'])  # database
+        self.module_builders = Module.make_module_builder_list(config['modules_directory'])  # module builders
+
+        # create instances of modules
+        for module_builder in self.module_builders:
+            self.modules[module_builder.name] = Module.new_instance(module_builder, self)
+            self.add_cog(self.modules[module_builder.name])
 
     def run(self):
         super().run(self.token)
